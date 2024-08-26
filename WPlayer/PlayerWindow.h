8e1c80c76a38418d625963da40766c1ff91efb6e
박재복 <pjb24@wizbrain.co.kr>
2024-08-26 11:23:44 +0900

    코드 정리
    - DESCRIPTION :
    
    사용하지 않는 D3D12Data, PlayerWindow 삭제함.
    빌드 옵션 수정함.
    
    - PROJECT : WPlayer
    - VERSION : 1.0.0.0
    - DATE : 2024-08-26 11:23
    - AUTHORS : Park Jaebok

--- WPlayer/PlayerWindow.h
+++
@@ -1,7 +0,0 @@
-//#pragma once
-//
-//#include "CommonHeaders.h"
-//
-//u32 create_window(WCHAR* window_class, WCHAR* title, HINSTANCE instance, RECT rect, void* data = nullptr, HWND& handle);
-
-//std::vector<HWND>   _window_handles;
